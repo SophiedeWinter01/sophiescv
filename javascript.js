692044944e672555a5d1806c9428e277277eabe9
Sophie <89905629+SophiedeWinter01@users.noreply.github.com>
2022-12-22 12:52:27 +0000

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -9,6 +9,6 @@ function validateForm() {
     if (name == "" || email == "" || problem1.checked == false && problem2.checked == false && problem3.checked == false) {
         alert("Everything must be filled in.")
     } else {
-        alert("Welcome" + name + "Your email is:" + email + "I will get back to you very soon!")
+        alert("Welcome" + '\xa0'+ name + '\xa0'+ "Your email is:"+'\xa0' + email + '\xa0'+ "I will get back to you very soon!")
     }
 }
